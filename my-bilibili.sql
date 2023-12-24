748cd12cb25790d89706e53e443a086cca24c947
tivvvvffffq <tivvvvffffq@gmail.com>
2023-12-25 09:36:39 +1100

    sql更新权限控制表

--- my-bilibili.sql
+++ my-bilibili.sql
@@ -61,3 +61,62 @@ CREATE TABLE `t_user_moments` (
     `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户动态表';
+
+DROP TABLE IF EXISTS `t_auth_element_operation`;
+CREATE TABLE `t_auth_element_operation` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `elementName` varchar(255) DEFAULT NULL COMMENT '页面元素名称',
+    `elementCode` varchar(50) DEFAULT NULL COMMENT '页面元素唯一编码',
+    `operationType` varchar(5) DEFAULT NULL COMMENT '操作类型：0可点击  1可见',
+    `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+    `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='权限控制--页面元素操作表';
+
+DROP TABLE IF EXISTS `t_auth_menu`;
+CREATE TABLE `t_auth_menu` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `name` varchar(255) DEFAULT NULL COMMENT '菜单项目名称',
+    `code` varchar(50) DEFAULT NULL COMMENT '唯一编码',
+    `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+    `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='权限控制-页面访问表';
+
+DROP TABLE IF EXISTS `t_auth_role`;
+CREATE TABLE `t_auth_role` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `name` varchar(255) DEFAULT NULL COMMENT '角色名称',
+    `code` varchar(50) DEFAULT NULL COMMENT '角色唯一编码',
+    `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+    `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='权限控制--角色表';
+
+DROP TABLE IF EXISTS `t_auth_role_element_operation`;
+CREATE TABLE `t_auth_role_element_operation` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `roleId` bigint DEFAULT NULL COMMENT '角色id',
+    `elementOperationId` bigint DEFAULT NULL COMMENT '元素操作id',
+    `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='权限控制--角色与元素操作关联表';
+
+DROP TABLE IF EXISTS `t_auth_role_menu`;
+CREATE TABLE `t_auth_role_menu` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `roleId` bigint DEFAULT NULL COMMENT '角色id',
+    `menuId` bigint DEFAULT NULL COMMENT '页面菜单id',
+    `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='权限控制--角色页面菜单关联表';
+
+DROP TABLE IF EXISTS `t_user_role`;
+CREATE TABLE `t_user_role` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `userId` bigint DEFAULT NULL COMMENT '用户id',
+    `roleId` bigint DEFAULT NULL COMMENT '角色id',
+    `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='用户角色关联表';
+
